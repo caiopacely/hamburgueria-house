8413cbb183f446d2d62b8a94c79c29ea8bec458e
caio pacely <caiopacely14@gmail.com>
2025-02-11 16:01:36 -0300

    aba dos itens

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const remove = document.getElementById("remove");
 const warning = document.getElementById("warning");
 const finalizeOrder = document.getElementById("finalize-order");
 const inputEddress = document.getElementById("inputEddress");
+const apenItem = document.getElementById("openItem");
 let cartProduct = [];   
 
 barraCart.addEventListener("click", function(){
@@ -117,6 +118,53 @@ inputEddress.addEventListener("input",function(action){
     }
 })
 
+function handleMenuClick(event) {
+    let selectProd = event.target.closest("#item");  
+    let notBtn = event.target.closest(".get-item");   
+    
+    if (selectProd && notBtn == null) {
+        const decriptionProd = document.createElement("div");
+        decriptionProd.innerHTML = `
+            <div id="modItem" class="w-full h-full bg-black/60 top-0 left-0 z-99 fixed flex justify-center items-center">       
+                <div class="w-[90%] max-w-xl rounded-xl bg-white flex flex-col gap-3">        
+                    <div class="px-4">
+                        <div class="flex justify-center items-center">
+                            <img class="max-h-96" src="${selectProd.getAttribute("src")}" alt="">
+                        </div>
+                        <div class="flex flex-col gap-3 mt-3">
+                            <p class="font-bold">${selectProd.getAttribute("dataName")}</p>
+                            <p class="text-gray-500">${selectProd.getAttribute("description")}</p>
+                            <p class="font-bold">R$ ${selectProd.getAttribute("dataPrice")}</p>
+                        </div>
+                    </div>
+                    <div class="w-full bg-gray-500 h-0.5"></div>
+                    <div class="p-2 w-full flex items-center justify-center gap-4">
+                        <div class="flex gap-6 bg-gray-200 py-2 px-6 text-xl rounded">
+                            <button>-</button>
+                            <p>0</p>
+                            <button>+</button>
+                        </div> 
+                        <div class="flex justify-between bg-black text-white p-2 rounded w-full">
+                            <p>Adicionar</p>
+                            <p>R$<span>0.00</span></p>
+                        </div>
+                    </div>         
+                </div>
+            </div>`;
+        
+        apenItem.appendChild(decriptionProd); 
+    }   
+}
+
+menu.addEventListener("click", handleMenuClick);
+menuBebidas.addEventListener("click", handleMenuClick);
+
+document.addEventListener("click", function(event){
+    let modalItem = document.getElementById("modItem");
+    if (event.target === modalItem) {
+        modalItem.remove(); 
+    }
+});
 
 
 finalizeOrder.addEventListener("click",function(){
