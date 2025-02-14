1ccd20101ff60c2230d48b9cd72389152a214b8b
caio pacely <caiopacely14@gmail.com>
2025-02-14 14:56:49 -0300

    modalItem adicinado

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const remove = document.getElementById("remove");
 const warning = document.getElementById("warning");
 const finalizeOrder = document.getElementById("finalize-order");
 const inputEddress = document.getElementById("inputEddress");
-const apenItem = document.getElementById("openItem");
+const openItem = document.getElementById("openItem");
 let cartProduct = [];   
 
 barraCart.addEventListener("click", function(){
@@ -24,40 +24,35 @@ modal.addEventListener("click",function(event){
     }
 })
 
-menu.addEventListener("click",function(event){   
-    let selectProd = event.target.closest(".get-item");   
-    
-    if(selectProd){
+function add(event){   
+    let selectProd = event.target.closest(".get-item"); 
+   
+    if(selectProd){       
+        selectProd.innerHTML= '<i class="fa-solid fa-plus"></i>'
+        selectProd.classList.add("atualizar")
         const name = selectProd.getAttribute("dataName");
         const price = selectProd.getAttribute("dataPrice");   
-        addProduct(name,price);
+        addProduct(name,price,1);
     }
-})
+}
 
-menuBebidas.addEventListener("click",function(event){   
-    let selectProd = event.target.closest(".get-item");
-    
-    if(selectProd){
-        const name = selectProd.getAttribute("dataName");
-        const price = selectProd.getAttribute("dataPrice");
-        addProduct(name,price);    
-    }
-})
+menu.addEventListener("click",add)
+menuBebidas.addEventListener("click",add)
 
-function addProduct(name,price){
+function addProduct(name,price,quantity){
     
     let produtoContem = cartProduct.find(prod => prod.name === name);
-    totalCart.innerHTML = (parseFloat(totalCart.innerHTML) + parseFloat(price)).toFixed(2);
+    totalCart.innerHTML = (parseFloat(totalCart.innerHTML) + parseFloat(price)*quantity).toFixed(2);
     
     if(produtoContem){
-        produtoContem.quantity ++;
+        produtoContem.quantity += quantity;
         upCart();
     }
     else{
         cartProduct.push({
             name,
             price,
-            quantity: 1,
+            quantity: quantity,
         });    
         upCart();
     }  
@@ -118,50 +113,96 @@ inputEddress.addEventListener("input",function(action){
     }
 })
 
-function handleMenuClick(event) {
+function creatModalItem(selectProd) {
+    const modalProductHtml = document.createElement("div");
+    modalProductHtml.innerHTML = `
+    <div id="modItem" class="w-full h-full bg-black/60 top-0 left-0 z-99 fixed flex justify-center items-center" dataName = "${selectProd.getAttribute("dataName")}" dataPrice = "${selectProd.getAttribute("dataPrice")}">       
+        <div class="w-[90%] max-w-xl rounded-xl bg-white flex flex-col gap-3">        
+            <div class="px-4">
+                <div class=" relative flex justify-center items-center ">
+                    <img class="max-h-96" src="${selectProd.getAttribute("src")}" alt="">
+                    <button id="closedBtnModal" class="bg-gray-200 w-10 h-10 text-gray-600 rounded-full absolute top-3 right-0 " ><i class="fa-solid fa-xmark"></i></button>
+                </div>
+                <div class="flex flex-col gap-3 mt-3">
+                    <p class="font-bold">${selectProd.getAttribute("dataName")}</p>
+                    <p class="text-gray-500">${selectProd.getAttribute("description")}</p>
+                    <p class="font-bold">R$ ${selectProd.getAttribute("dataPrice")}</p>
+                </div>
+            </div>
+            <div class="w-full bg-gray-500 h-0.5"></div>
+            <div class="p-2 w-full flex items-center justify-center gap-4">
+                <div id="boxQtd" class="flex gap-6 bg-gray-200 py-2 px-6 text-xl rounded">
+                    <button id="decrease">-</button>
+                    <p id="quantity">1</p>
+                    <button id="increase">+</button>
+                </div> 
+                <div id="addForModalItem" class="flex justify-between bg-black text-white p-2 rounded w-full">
+                    <p >Adicionar</p>
+                    <p>R$<span id="totalItem">${selectProd.getAttribute("dataPrice")}</span></p>
+                </div>
+            </div>         
+        </div>
+    </div>`;
+
+    return modalProductHtml;
+}   
+
+function itemClick(event) {
     let selectProd = event.target.closest("#item");  
     let notBtn = event.target.closest(".get-item");   
     
     if (selectProd && notBtn == null) {
-        const decriptionProd = document.createElement("div");
-        decriptionProd.innerHTML = `
-            <div id="modItem" class="w-full h-full bg-black/60 top-0 left-0 z-99 fixed flex justify-center items-center">       
-                <div class="w-[90%] max-w-xl rounded-xl bg-white flex flex-col gap-3">        
-                    <div class="px-4">
-                        <div class="flex justify-center items-center">
-                            <img class="max-h-96" src="${selectProd.getAttribute("src")}" alt="">
-                        </div>
-                        <div class="flex flex-col gap-3 mt-3">
-                            <p class="font-bold">${selectProd.getAttribute("dataName")}</p>
-                            <p class="text-gray-500">${selectProd.getAttribute("description")}</p>
-                            <p class="font-bold">R$ ${selectProd.getAttribute("dataPrice")}</p>
-                        </div>
-                    </div>
-                    <div class="w-full bg-gray-500 h-0.5"></div>
-                    <div class="p-2 w-full flex items-center justify-center gap-4">
-                        <div class="flex gap-6 bg-gray-200 py-2 px-6 text-xl rounded">
-                            <button>-</button>
-                            <p>0</p>
-                            <button>+</button>
-                        </div> 
-                        <div class="flex justify-between bg-black text-white p-2 rounded w-full">
-                            <p>Adicionar</p>
-                            <p>R$<span>0.00</span></p>
-                        </div>
-                    </div>         
-                </div>
-            </div>`;
-        
-        apenItem.appendChild(decriptionProd); 
+        const estructItem = creatModalItem(selectProd)
+
+        openItem.appendChild(estructItem);
+
     }   
 }
 
-menu.addEventListener("click", handleMenuClick);
-menuBebidas.addEventListener("click", handleMenuClick);
+menu.addEventListener("click", itemClick);
+menuBebidas.addEventListener("click", itemClick);
+
+
+
+document.addEventListener("click", function(event){
+    let increase = document.getElementById("increase")
+    let decrease = document.getElementById("decrease")
+    let quantity = document.getElementById("quantity")
+    let total = document.getElementById("totalItem")
+    let modItem = document.getElementById("modItem")
+    let price = modItem.getAttribute("dataPrice")
+
+    if(event.target === increase){
+        quantity.innerHTML = Number(quantity.innerHTML) + 1
+        total.innerHTML = (Number(total.innerHTML) + Number(price)).toFixed(2)
+        
+    }
+    else if(event.target === decrease && Number(total.innerHTML)>price){
+        quantity.innerHTML = Number(quantity.innerHTML) - 1
+        total.innerHTML = (Number(total.innerHTML) - Number(price)).toFixed(2)
+    } 
+})
+
+
+document.addEventListener("click", function(event){
+    let addForModalItem = event.target.closest("#addForModalItem")
+    let quantity = document.getElementById("quantity")
+    
+    if(addForModalItem){
+        let modItem = document.getElementById("modItem")
+        const name = modItem.getAttribute("dataName")
+        const price = modItem.getAttribute("dataPrice")
+        addProduct(name,price,Number(quantity.innerHTML))
+        modItem.remove()
+    } 
+   
+})
+  
 
 document.addEventListener("click", function(event){
     let modalItem = document.getElementById("modItem");
-    if (event.target === modalItem) {
+    let cloasedModalItem = document.getElementById("closedBtnModal");
+    if (event.target === modalItem || event.target=== cloasedModalItem ) {
         modalItem.remove(); 
     }
 });
